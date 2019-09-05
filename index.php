e430ab9dae8bf703b953dceb7512acfe26cc7a5a
Demeter Cristin <contact@demetercristin.fr>
2019-09-05 10:30:00 +0200

    add paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ echo "Hello";
 
 ?>
 <h1>bases git</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto harum consectetur non! Culpa, illo quasi rerum reprehenderit quisquam eius ipsam?</p>
 
 <?php
