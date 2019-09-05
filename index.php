f41f4fbf40059e7aa286dec41102e6cea0cbd071
Demeter Cristin <contact@demetercristin.fr>
2019-09-05 11:12:13 +0200

    Revert "petit update - bug detecte"
    This reverts commit 90d8b1fbfb3ab469aa926300e9a77be6dd2db848.

--- index.php
+++ index.php
@@ -10,5 +10,3 @@ echo "Hello";
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto harum consectetur non! Culpa, illo quasi rerum reprehenderit quisquam eius ipsam?</p>
 
 <?php
-
-echo "bug";
